ec489aa407719597beb48ff9dabd531eab0d5c3f
Till Krüss <till@kruss.io>
2021-12-02 18:22:07 -0800

    php 8.1 packages

--- build/fpm/fpm.sh
+++ build/fpm/fpm.sh
@@ -9,30 +9,40 @@ baseurl="https://cachewerk.s3.amazonaws.com/relay/$version/relay-$version"
 
 packages=(
   "deb base       amd64  8.0 20200930 $baseurl-php8.0-debian-x86-64.tar.gz"
+  "deb multi      amd64  8.1 20210902 $baseurl-php8.1-debian-x86-64.tar.gz"
   "deb multi      amd64  8.0 20200930 $baseurl-php8.0-debian-x86-64.tar.gz"
   "deb multi      amd64  7.4 20190902 $baseurl-php7.4-debian-x86-64.tar.gz"
 
   "deb base       arm64  8.0 20200930 $baseurl-php8.0-debian-aarch64.tar.gz"
+  "deb multi      arm64  8.1 20210902 $baseurl-php8.1-debian-aarch64.tar.gz"
   "deb multi      arm64  8.0 20200930 $baseurl-php8.0-debian-aarch64.tar.gz"
   "deb multi      arm64  7.4 20190902 $baseurl-php7.4-debian-aarch64.tar.gz"
 
+  "rpm single.el7 x86_64 8.1 20210902 $baseurl-php8.1-centos7-x86-64.tar.gz"
   "rpm single.el7 x86_64 8.0 20200930 $baseurl-php8.0-centos7-x86-64.tar.gz"
   "rpm single.el7 x86_64 7.4 20190902 $baseurl-php7.4-centos7-x86-64.tar.gz"
+  "rpm multi.el7  x86_64 8.1 20210902 $baseurl-php8.1-centos7-x86-64.tar.gz"
   "rpm multi.el7  x86_64 8.0 20200930 $baseurl-php8.0-centos7-x86-64.tar.gz"
   "rpm multi.el7  x86_64 7.4 20190902 $baseurl-php7.4-centos7-x86-64.tar.gz"
 
+  "rpm single.el7 aarch64 8.1 20210902 $baseurl-php8.1-centos7-aarch64.tar.gz"
   "rpm single.el7 aarch64 8.0 20200930 $baseurl-php8.0-centos7-aarch64.tar.gz"
   "rpm single.el7 aarch64 7.4 20190902 $baseurl-php7.4-centos7-aarch64.tar.gz"
+  "rpm multi.el7  aarch64 8.1 20210902 $baseurl-php8.1-centos7-aarch64.tar.gz"
   "rpm multi.el7  aarch64 8.0 20200930 $baseurl-php8.0-centos7-aarch64.tar.gz"
   "rpm multi.el7  aarch64 7.4 20190902 $baseurl-php7.4-centos7-aarch64.tar.gz"
 
+  "rpm single.el8 x86_64 8.1 20210902 $baseurl-php8.1-centos8-x86-64.tar.gz"
   "rpm single.el8 x86_64 8.0 20200930 $baseurl-php8.0-centos8-x86-64.tar.gz"
   "rpm single.el8 x86_64 7.4 20190902 $baseurl-php7.4-centos8-x86-64.tar.gz"
+  "rpm multi.el8  x86_64 8.1 20210902 $baseurl-php8.1-centos8-x86-64.tar.gz"
   "rpm multi.el8  x86_64 8.0 20200930 $baseurl-php8.0-centos8-x86-64.tar.gz"
   "rpm multi.el8  x86_64 7.4 20190902 $baseurl-php7.4-centos8-x86-64.tar.gz"
 
+  "rpm single.el8 aarch64 8.1 20210902 $baseurl-php8.1-centos8-aarch64.tar.gz"
   "rpm single.el8 aarch64 8.0 20200930 $baseurl-php8.0-centos8-aarch64.tar.gz"
   "rpm single.el8 aarch64 7.4 20190902 $baseurl-php7.4-centos8-aarch64.tar.gz"
+  "rpm multi.el8  aarch64 8.1 20210902 $baseurl-php8.1-centos8-aarch64.tar.gz"
   "rpm multi.el8  aarch64 8.0 20200930 $baseurl-php8.0-centos8-aarch64.tar.gz"
   "rpm multi.el8  aarch64 7.4 20190902 $baseurl-php7.4-centos8-aarch64.tar.gz"
 )
