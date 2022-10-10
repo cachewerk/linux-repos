480c413534961a7749f9466197478eceddde4455
Till Krüss <till@kruss.io>
2022-10-10 11:12:47 -0700

    switch to new Nil UUID, keep old replacement

--- build/fpm/src/rpm/after-install.sh
+++ build/fpm/src/rpm/after-install.sh
@@ -5,6 +5,7 @@ set -e
 uuid=$(uuidgen -r)
 
 <% binary_paths.split(' ').each do |binary_path| -%>
+sed -i "s/00000000-0000-0000-0000-000000000000/${uuid}/" <%= binary_path %>/relay.so
 sed -i "s/BIN:31415926-5358-9793-2384-626433832795/BIN:${uuid}/" <%= binary_path %>/relay.so
 <% end -%>
 
