e61716f999f2838031a13d1eb75c5db22614954d
Till Krüss <tillkruss@users.noreply.github.com>
2024-11-25 09:50:08 -0800

    update uuid

--- build/fpm/src/rpm/after-install.sh
+++ build/fpm/src/rpm/after-install.sh
@@ -6,7 +6,6 @@ uuid=$(uuidgen -r)
 
 <% binary_paths.split(' ').each do |binary_path| -%>
 sed -i "s/00000000-0000-0000-0000-000000000000/${uuid}/" <%= binary_path %>/relay.so
-sed -i "s/BIN:31415926-5358-9793-2384-626433832795/BIN:${uuid}/" <%= binary_path %>/relay.so
 <% end -%>
 
 exit 0
